f8bfbc5b85fe493836ecd4522820b1a9d7f471a0
avcton <avcton@gmail.com>
2023-03-11 16:20:06 +0500

    spes

--- src/utils/navBar.jsx
+++ src/utils/navBar.jsx
@@ -7,7 +7,7 @@ function NavBar(){
         <div className={` ${open? "w-72" : "w-20"} z-10 bg-white duration-300 h-screen shadow-2xl relative`}>
             <img 
                 src={icon}
-                className= {` absolute cursor-pointer duration-500 rounded-full ${open? "rotate-180": "rotate-0"}
+                className= {` absolute cursor-pointer duration-500 rounded-full ${!open? "rotate-180": "rotate-0"}
                 -right-3 top-14 border-4 border-white`}
                 onClick={()=> setOpen(!open)}
             />
